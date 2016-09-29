8b2b8ee72fd01fff9ac5ee25a9c0db4699a63c71
yaropower <yaroslaman@gmail.com>
2016-09-29 20:48:38 +0300

    optimized method StringOperation

--- src/kareta/laboratoryworks/lab2/StringOperation.java
+++ src/kareta/laboratoryworks/lab2/StringOperation.java
@@ -3,6 +3,7 @@ package kareta.laboratoryworks.lab2;
 import java.util.Scanner;
 
 public class StringOperation {
+<<<<<<< c4d220349b04886bb0ce60109b847381720f6c1e
 
     public static String replaceWords(String text, int checkedLength) {
         StringBuffer buffer = new StringBuffer(text);
@@ -12,21 +13,42 @@ public class StringOperation {
         while (wordStart < buffer.length()) {
             int wordEnd = wordStart;
 
+=======
+    //i think that's working great
+    public static String replaceWords(String text, int checkedLength) {
+        StringBuffer buffer = new StringBuffer(text);
+        final String consonants = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvXxZzWwYy";
+        int wordStart = 0;
+        while (wordStart < buffer.length()) {
+            int wordEnd=wordStart;
+>>>>>>> optimized method StringOperation
             if (Character.isLetter(buffer.charAt(wordEnd))) {
                 while (wordEnd < buffer.length() && Character.isLetter(buffer.charAt(wordEnd))) {
                     wordEnd++;
                 }
+<<<<<<< c4d220349b04886bb0ce60109b847381720f6c1e
 
+=======
+>>>>>>> optimized method StringOperation
                 boolean rightLength = (wordEnd - wordStart) == checkedLength;
                 boolean firstIsConsonant = consonants.contains(Character.toString(buffer.charAt(wordStart)));
                 if (rightLength && firstIsConsonant) {
                     buffer.delete(wordStart, wordEnd);
+<<<<<<< c4d220349b04886bb0ce60109b847381720f6c1e
                 }
             }
             wordStart = wordEnd;
             wordStart++;
         }
 
+=======
+                    wordStart=wordEnd-checkedLength;
+                }
+                else wordStart=wordEnd;
+            }
+            wordStart++;
+        }
+>>>>>>> optimized method StringOperation
         return buffer.toString();
     }
 
@@ -36,7 +58,10 @@ public class StringOperation {
         String line = input.nextLine();
         System.out.println("Words length to be deleted:");
         int wordLengthToDelete = input.nextInt();
+<<<<<<< c4d220349b04886bb0ce60109b847381720f6c1e
 
+=======
+>>>>>>> optimized method StringOperation
         String resultLine = replaceWords(line, wordLengthToDelete);
         System.out.println("Result:");
         System.out.println(resultLine);
