528d770e6dc4cf8742983a103304d7ed5ff10307
victor <waterwolf@ukr.net>
2016-11-10 10:31:19 +0300

    Create base controller

--- src/kareta/consoleui/Controller.java
+++ src/kareta/consoleui/Controller.java
@@ -1,8 +1,26 @@
 package kareta.consoleui;
 
-/**
- * Created by vitya on 07.11.2016.
- */
 public abstract class Controller {
+    private ViewRenderer viewRenderer = new ViewRenderer();
+    private View view;
 
+    protected Controller(View view) {
+        this.view = view;
+    }
+
+    public void run()
+    {
+        boolean exit = false;
+        while (!exit)
+        {
+            try {
+                String choice = viewRenderer.run(view);
+                exit = runChoice(choice);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public abstract boolean runChoice(String choice);
 }
