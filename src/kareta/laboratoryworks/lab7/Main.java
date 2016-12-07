fa0c8b64b4f931bbca590b9b9ef3789798c9e9dd
kareta <karetavictor@gmail.com>
2016-12-08 01:07:53 +0200

    Update 7-th laboratory work

--- src/kareta/laboratoryworks/lab7/Main.java
+++ src/kareta/laboratoryworks/lab7/Main.java
@@ -4,23 +4,27 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Type path to source and destination file: ");
-        String sourcePath  = console.next();
-        String destinationPath  = console.next();
 
+    public static void main(String[] args) {
+
+    handleExceptions();
+
+///home/vitya/matrix /home/vitya/matrixcopy
+    }
+
+    public static void handleExceptions() {
         Scanner in = null;
         BufferedWriter out = null;
 
         try {
+            Scanner console = new Scanner(System.in);
+            System.out.println("Type path to source and destination file: ");
+            String sourcePath  = console.next();
+            String destinationPath  = console.next();
+
             in = new Scanner(new FileInputStream(sourcePath));
             out = new BufferedWriter(new FileWriter(destinationPath));
-
-            while (in.hasNextInt()) {
-                int number = in.nextInt();
-                System.out.println(number);
-            }
+            run(in, out);
         } catch (FileNotFoundException e) {
             System.out.println("Incorrect path");
         } catch (IOException e) {
@@ -33,6 +37,37 @@ public class Main {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    public static void run(Scanner in, BufferedWriter out) throws IOException {
+
+        int rowsAmount = in.nextInt();
+        int columnsAmount = in.nextInt();
+
+        MatrixTransposer transposer = new MatrixTransposer();
+        int[][] matrix = transposer.getEmptyMatrix(rowsAmount, columnsAmount);
+
+        for (int i = 0; i < rowsAmount; i++) {
+            for (int j = 0; j < columnsAmount; j++) {
+                if (in.hasNextInt()) {
+                    matrix[i][j] = in.nextInt();
+                }
+            }
+        }
+
+        transposer.print(matrix);
+        matrix = transposer.transpose(matrix);
+        System.out.println();
+        transposer.print(matrix);
+
+        out.write(rowsAmount + " " + columnsAmount + "\n");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                out.write(matrix[i][j] + " ");
+            }
+            out.newLine();
         }
     }
 }
--- src/kareta/laboratoryworks/lab7/MatrixTransposer.java
+++ src/kareta/laboratoryworks/lab7/MatrixTransposer.java
@@ -1,20 +1,44 @@
 package kareta.laboratoryworks.lab7;
 
 public class MatrixTransposer {
-    public void transpose(int[][] matrix) {
+
+    public int[][] getEmptyMatrix(int rowsAmount, int columnsAmount) {
+        int[][] matrix = new int[rowsAmount][];
+
+        for (int i = 0; i < rowsAmount; i++) {
+            matrix[i] = new int[columnsAmount];
+        }
+
+        return matrix;
+    }
+
+    public int[][] transpose(int[][] matrix) {
         if (matrix == null) {
-            return;
+            return matrix;
         }
 
         int[][] transposedMatrix = new int[matrix[0].length][];
-        for (int[] row : transposedMatrix) {
-            row = new int[matrix.length];
+        for (int i = 0; i < matrix[0].length; i++) {
+            transposedMatrix[i] = new int[matrix.length];
         }
 
+
         for (int i = 0; i < transposedMatrix.length; i++) {
-            for (int j = 0; j < transposedMatrix.length; j++) {
+            for (int j = 0; j < transposedMatrix[0].length; j++) {
                 transposedMatrix[i][j] = matrix[j][i];
             }
+        }
+
+
+        return transposedMatrix;
+    }
+
+    public void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
         }
     }
 }
